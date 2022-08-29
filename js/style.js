32cd88cbf80814e151ef56be9c1988fd7716dcc4
satohi0000 <87967374+satohi0000@users.noreply.github.com>
2022-08-30 01:29:34 +0900

    ヘッダー調整

--- js/style.js
+++ js/style.js
@@ -5,3 +5,23 @@ $("#c-BTN1").on("click", function () {
   $("body").toggleClass("no_scroll");
   return false;
 });
+
+$(function () {
+  var $win = $(window),
+    $main = $("main"),
+    $nav = $("l-header-inner"),
+    navHeight = $nav.outerHeight(),
+    navPos = $nav.offset().top,
+    fixedClass = "is-fixed";
+
+  $win.on("load scroll", function () {
+    var value = $(this).scrollTop();
+    if (value > navPos) {
+      $nav.addClass(fixedClass);
+      $main.css("margin-top", navHeight);
+    } else {
+      $nav.removeClass(fixedClass);
+      $main.css("margin-top", "0");
+    }
+  });
+});
