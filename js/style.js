4c80340ec016b9d831c765c9d6abd1863c9faea3
satohi0000 <87967374+satohi0000@users.noreply.github.com>
2022-08-31 12:02:22 +0900

    fv動かず

--- js/style.js
+++ js/style.js
@@ -8,20 +8,25 @@ $("#c-BTN1").on("click", function () {
 
 $(function () {
   var $win = $(window),
-    $main = $("main"),
-    $nav = $("l-header-inner"),
-    navHeight = $nav.outerHeight(),
-    navPos = $nav.offset().top,
-    fixedClass = "is-fixed";
+    $fv = $(".p-fv"),
+    $header = $(".l-header");
+  (fvHeight = $fv.outerHeight()), (fixedClass = "is-fixed");
 
   $win.on("load scroll", function () {
     var value = $(this).scrollTop();
-    if (value > navPos) {
-      $nav.addClass(fixedClass);
-      $main.css("margin-top", navHeight);
-    } else {
-      $nav.removeClass(fixedClass);
-      $main.css("margin-top", "0");
-    }
+    if ($win.width() > 768)
+      if (value > fvHeight) {
+        $header.addClass(fixedClass);
+      } else {
+        $header.removeClass(fixedClass);
+      }
   });
+});
+
+$(".p-fv__slider").slick({
+  autoplay: true,
+  autoplaySpeed: 2000,
+  speed: 1000,
+  fade: true,
+  cssEase: "linear",
 });
